da70eb0c4e2125f23b0a063b1ecdc4f013e7bf33
namneet8 <namneettung123@gmail.com>
2025-01-12 01:57:00 -0500

    updated code for clarity

--- Day1/4_Variables_LenMethod.py
+++ Day1/4_Variables_LenMethod.py
@@ -12,17 +12,24 @@ print()
 #print("The length of user's input is " + len(input("What is your name?")))
 # above line will give error because when we do concatenation, it expects a string but len function returns an int
 print(len(input("What is your name?")))
+print()
 
 #creating separate variables for above step
 username = input("What is your name?")
 length = len(username)
 print(length)
+print()
+
+#len(123456) won't work because length method only accepts Strings
 
 #We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. 
 # #Write 3 lines of code to switch the contents of the variables. 
 # You are not allowed to type the words "milk" or "juice". You are only allowed to use variables to solve this exercise.
 glass1 = "milk"
 glass2 = "juice"
+
 temp = "milk"
 glass1 = glass2
 glass2 = temp
+print(glass1)
+print(glass2)
