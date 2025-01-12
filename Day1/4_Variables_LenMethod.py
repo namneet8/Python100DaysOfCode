7544a0e43a55a0152ee029836ba33fd88755585c
namneet8 <namneettung123@gmail.com>
2025-01-12 01:38:44 -0500

    Completed Day 1 Exercise 4

---
+++ Day1/4_Variables_LenMethod.py
@@ -0,0 +1,28 @@
+#creating a variable and storing user's input
+name = input("What is your name?")
+print(name)
+print()
+
+#the value in a variable can be changed at any time of the code
+name = "Tom"
+print(name)
+print()
+
+#using the length function to check length of user's input
+#print("The length of user's input is " + len(input("What is your name?")))
+# above line will give error because when we do concatenation, it expects a string but len function returns an int
+print(len(input("What is your name?")))
+
+#creating separate variables for above step
+username = input("What is your name?")
+length = len(username)
+print(length)
+
+#We have 2 variables glass1 and glass2. glass1 contains milk and glass2 contains juice. 
+# #Write 3 lines of code to switch the contents of the variables. 
+# You are not allowed to type the words "milk" or "juice". You are only allowed to use variables to solve this exercise.
+glass1 = "milk"
+glass2 = "juice"
+temp = "milk"
+glass1 = glass2
+glass2 = temp
