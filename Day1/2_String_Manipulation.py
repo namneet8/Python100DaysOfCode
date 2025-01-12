774a0421062334d3ca32764ef564eef416ea287e
namneet8 <namneettung123@gmail.com>
2025-01-12 01:10:10 -0500

    Added String Concatenation

--- Day1/2_String_Manipulation.py
+++ Day1/2_String_Manipulation.py
@@ -7,4 +7,6 @@ print("My name is 'Namneet'") #either use double quotes outside and single quote
 #or vice-versa or use special characters like below
 print("My name is \"Namneet\"")
 
-
+# use '+' to concatenate strings (both should be strings) 
+print("Hello" + " " + "Namneet!")
+# print("Hello" + 5) will give error
