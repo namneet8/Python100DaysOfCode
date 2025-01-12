81dd29d7520b450facf48d1709d9c36b028b4ea2
namneet8 <namneettung123@gmail.com>
2025-01-12 01:45:28 -0500

    Completed Day 1 Final Exercise

---
+++ Day1/6_Band_Name_Generator.py
@@ -0,0 +1,8 @@
+# Create a greeting for your program.
+print("Hi! Welcome to Band name Generator.")
+# Ask the user for the city that they grew up in and store it in a variable.
+city = input("Which city did you grew up in?\n")
+# Ask the user for the name of a pet and store it in a variable.
+pet = input("What is your pet's name?\n")
+# Combine the name of their city and pet and show them their band name.
+print("Suggested Band name: " + city + " " + pet)
