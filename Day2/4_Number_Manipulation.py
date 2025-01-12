de46a607496c88d1a49cafdcbfb1bd7b551a20ab
namneet8 <namneettung123@gmail.com>
2025-01-12 02:17:13 -0500

    Completed Day 2 Exercise 4

---
+++ Day2/4_Number_Manipulation.py
@@ -0,0 +1,21 @@
+#Flooring a number
+print(int(3.72345))
+
+#Rounding a number
+print(round(3.723456))
+
+#round(number, ndigits)
+bmi = 84 / 1.65 ** 2
+print(round(bmi, 3))
+
+score = 1
+score += 1
+score -= 1
+score *= 1
+score /= 1
+
+#f-strings
+age = 12
+print(f"I am {age} years old")
+
+# Will output I am 12 years old.
