666af0177ab8bbc98fe2301d99db209a13de7c5b
namneet8 <namneettung123@gmail.com>
2025-01-12 01:06:08 -0500

    learnt special characters

---
+++ Day1/2_String_Manipulation.py
@@ -0,0 +1,10 @@
+# \n -> new line character
+# \' -> for single quote
+# \" -> for double quote
+
+print("My name is Namneet.\nI am new to Python.")
+print("My name is 'Namneet'") #either use double quotes outside and single quotes inside string 
+#or vice-versa or use special characters like below
+print("My name is \"Namneet\"")
+
+
