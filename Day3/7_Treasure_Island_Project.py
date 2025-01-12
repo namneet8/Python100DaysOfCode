ca183151eb569281d5bb6f1be0aa40e9f752a540
namneet8 <namneettung123@gmail.com>
2025-01-12 14:47:12 -0500

    added link to flow chart

--- Day3/7_Treasure_Island_Project.py
+++ Day3/7_Treasure_Island_Project.py
@@ -1,5 +1,7 @@
 #notice the use of print ('''   '''), upper(), lower()
-#see the flow chart for this exercise
+
+#see the following flow chart for this exercise-
+#https://app.diagrams.net/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload#%7B%22pageId%22%3A%22C5RBs43oDa-KdzZeNtuy%22%7D
 
 
 print('''
