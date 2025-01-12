91ff7a32cc60dae09fc355416bf1888dc9ce229b
namneet8 <namneettung123@gmail.com>
2025-01-12 02:32:23 -0500

    Completed Day 3 Exercise 2

---
+++ Day3/2_Modulo.py
@@ -0,0 +1,9 @@
+#What do you think this will output?
+print(10 % 3) #1
+
+#Check Odd or Even
+number = int(input("Please Enter an Integer:\n"))
+if number % 2 == 0:
+    print("You entered an Even number")
+else:
+    print("You entered an odd number")
