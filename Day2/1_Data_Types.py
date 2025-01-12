ad0d2d866cc1b421c10066d84a2456f988b12baf
namneet8 <namneettung123@gmail.com>
2025-01-12 02:00:26 -0500

    Completed Day 2 Exercise 1

---
+++ Day2/1_Data_Types.py
@@ -0,0 +1,18 @@
+#data types
+print("I am a String") #String (str)
+print(1) #Integer (int)
+print(123_456_789) #Integer (int)
+print(3.1456) #floating point (float)
+print(True) #Boolean (bool)
+print(False) #Boolean (bool)
+print()
+
+#print("Hello"[4]) #will print o from hello, 0->H, 1 -> e, 2,3-> l, 4 -> 0
+# we can also use -ve numbers as indices
+print("Hello"[-1])
+print("Hello"[-2])
+print("Hello"[-3])
+print("Hello"[-4])
+print("Hello"[-5])
+#print("Hello"[-6]) # this will produce error: string index out of range
+
