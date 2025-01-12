32b08f86a3db83e5585c5938347e0d154b227ae1
namneet8 <namneettung123@gmail.com>
2025-01-12 00:53:44 -0500

    addding first file

---
+++ Day1/1_Print_Statements.py
@@ -0,0 +1,30 @@
+# In python print() is used to for printing
+
+print("Hello World!!")
+print()
+
+#printing a recipe using multiple print statements
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+print()
+
+#printing multiple lines (in IDE) using only one print method
+print('''1. Python is a popular, high-level programming language known for its simplicity and versatility.  
+2. It was created by Guido van Rossum and first released in 1991.  
+3. Python emphasizes code readability with its clear and concise syntax.  
+4. The language supports multiple programming paradigms, including object-oriented, functional, and procedural programming.  
+5. Python is widely used in various fields like web development, data analysis, artificial intelligence, and scientific computing.  
+6. Its extensive standard library includes modules for tasks like file handling, networking, and data visualization.  
+7. Popular Python frameworks and libraries include Django, Flask, NumPy, and TensorFlow.  
+8. Python’s community is large and active, providing ample resources for learning and problem-solving.  
+9. It is compatible with major platforms and operating systems, ensuring cross-platform development.  
+10. Python’s simplicity and power make it an excellent choice for beginners and experienced developers alike.  ''')
+print()
+
+#another way to print multiple lines but need to add \n character as compared to above method
+print("1. Python is a popular, high-level programming language known for its simplicity and versatility.\n" 
+"2. It was created by Guido van Rossum and first released in 1991.\n" 
+"3. Python emphasizes code readability with its clear and concise syntax.\n")
