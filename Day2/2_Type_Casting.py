98edbb760ea4557a6ce06d5baf233c474f6e3bfe
namneet8 <namneettung123@gmail.com>
2025-01-12 02:03:58 -0500

    Completed Day 2 Exercise 2

---
+++ Day2/2_Type_Casting.py
@@ -0,0 +1,12 @@
+#Type Checking
+print(type(123_456))
+print(type(123))
+print(type("Tom"))
+print(type(True))
+print(type(123_56.234))
+print()
+
+#Type Conversion
+# Make this line of code run without errors
+# print("Number of letters in your name: " + len(input("Enter your name")))
+print("Number of letters in your name: " + str(len(input("Enter your name\n"))))
