b879275e922622b280881298c96acad9740b9e2c
namneet8 <namneettung123@gmail.com>
2025-01-16 21:24:03 -0500

    Day 1 Exercise 1 Completed

---
+++ Day5/1_CreatingFunctions.py
@@ -0,0 +1,16 @@
+# Defining a new Function
+# def <function name>():
+#     print("Hello")
+
+# Calling a Function
+# <function name>()
+
+#Creating the function
+def get_user_name():
+    name = input("What is your name? ")
+    print("Hello, " + name)
+    # Inside the function
+
+#Outside the function
+print("Hello")
+get_user_name() # Calling the function
