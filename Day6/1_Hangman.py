1baebdcc7971c8cc0b09a8da656be0fefa4c887c
namneet8 <namneettung123@gmail.com>
2025-01-18 22:58:49 -0500

    chages

--- Day6/1_Hangman.py
+++ Day6/1_Hangman.py
@@ -5,11 +5,4 @@
 #                                                     -3
 #                                                      1    -2 - (-3) = 1
 # Case 2: Comp wins
-# i) Comp - 0 User-2 -> (2-0)%3 = 2%3 = 2
-# ii) Comp- 1 User-0 -> (0-1)%3 = -1%3 = 2
-# iii) Comp-2 User-1 -> (1-2)%3 = -1%3 = 2         -1 - (-3) = 2
-# 
-# Case 3: Draw
-# i) Comp - 0 User-0 -> 0%3 = 0
-# ii) Comp- 1 User-1 -> 0%3 = 0
-# iii) Comp-1 User-1 -> 0%3 = 0  
+# i) Comp
