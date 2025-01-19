010f3239b82a56bcea6c89399ea8733bfa663d06
namneet8 <namneettung123@gmail.com>
2025-01-18 22:59:20 -0500

    cleared unnecesaary files

--- Day6/1_Hangman.py
+++
@@ -1,8 +0,0 @@
-# Case 1: User wins
-# i) Comp - 0 User-1 -> (1-0)%3 = 1%3 = 1
-# ii) Comp- 1 User-2 -> (2-1)%3 = 1%3 = 1
-# iii) Comp-2 User-0 -> (0-2)%3 = -2%3 = 1          3)-2(-1
-#                                                     -3
-#                                                      1    -2 - (-3) = 1
-# Case 2: Comp wins
-# i) Comp
